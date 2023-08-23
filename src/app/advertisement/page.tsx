ed2f7248f9543fd6eba3d7b51a7015c2cf529fa6
peach733 <kimjeje15@gmail.com>
2023-08-23 15:32:48 +0900

    Update: 광고 페이지 추가

---
+++ src/app/advertisement/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutPage from '../../common/components/organisms/AboutPage';
+import { PageHeader } from '../../common/components/organisms/PageHeader/index';
+
+const advertisement = () => {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default advertisement;
