b48e8fae3d6bd2a5f474cf997e1b28416dcdf37e
kimjuyong <kimjudragon@gmail.com>
2025-11-05 14:25:23 +0900

    배포

--- src/common/components/molecules/NavMenu/navMenu.style.ts
+++ src/common/components/molecules/NavMenu/navMenu.style.ts
@@ -1,21 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import {
-    width,
-    height,
-    color,
-    space,
-    border,
-    boxShadow,
-    borderRadius
-} from 'styled-system';
+  width,
+  height,
+  color,
+  space,
+  border,
+  boxShadow,
+  borderRadius,
+} from "styled-system";
 
 const NavMenuWrapper = styled.ul`
-display: flex;
-width: 640px;
-height: 70px;
-align-items: center;
+  display: flex;
+  width: 640px;
+  height: 70px;
+  align-items: center;
 
-> li {
+  > li {
     display: flex;
     width: 160px;
     height: 70px;
@@ -23,32 +23,32 @@ align-items: center;
     justify-content: center;
 
     &.active {
-        a {
-            color: #6194F4;
-            font-weight: bold;
-        }
+      a {
+        color: #6194f4;
+        font-weight: bold;
+      }
     }
-    
+
     &:hover {
-        a {
-            color: #6194F4;
-            font-weight: bold;
-            border-bottom: 3px solid #6194F4;
-        }
+      a {
+        color: #6194f4;
+        font-weight: bold;
+        border-bottom: 3px solid #6194f4;
+      }
     }
 
     > a {
-        display: inline-block;
-        width: 100%;
-        height: 100%;
-        text-align: center;
-        line-height: 70px;
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      line-height: 70px;
     }
-}
-`
+  }
+`;
 
 const DeptMenuWrapper = styled.ul`
-> li {
+  > li {
     display: flex;
     width: 160px;
     height: 50px;
@@ -56,32 +56,32 @@ const DeptMenuWrapper = styled.ul`
     justify-content: center;
 
     &.active {
-        a {
-            color: #fff;
-            font-weight: bold;
-            background: #6194F4;
-        }
+      a {
+        color: #fff;
+        font-weight: bold;
+        background: #6194f4;
+      }
     }
 
     &:hover {
-        a {
-            color: #fff;
-            font-weight: bold;
-            background: #4077DD;
-        }
+      a {
+        color: #fff;
+        font-weight: bold;
+        background: #4077dd;
+      }
     }
 
     > a {
-        display: inline-block;
-        width: 100%;
-        height: 100%;
-        text-align: center;
-        line-height: 50px;
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      line-height: 50px;
     }
-}
-`
+  }
+`;
 
-NavMenuWrapper.displayName = 'NavMenuWrapper';
-DeptMenuWrapper.displayName = 'DeptMenuWrapper';
+NavMenuWrapper.displayName = "NavMenuWrapper";
+DeptMenuWrapper.displayName = "DeptMenuWrapper";
 
 export { NavMenuWrapper, DeptMenuWrapper };
